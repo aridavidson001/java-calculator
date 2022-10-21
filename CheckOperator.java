4a494aedb87c6180a4442507f1c9730346b89373
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-10-21 14:16:25 -0400

    Update CheckOperator.java

--- CheckOperator.java
+++ CheckOperator.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 
 class CheckOperator {
   public static void main(String[] args) {}
-  public static boolean checkOperator(String userInputFunction) {
-    // Create a Scanner object
+  public static void checkOperatorFunc(String userInputFunction) {
     Scanner userInputScannerObj = new Scanner(System.in);
+    if (checkOperator(userInputFunction) == false) {
+      while (checkOperator(userInputFunction) == false) {
+        System.out.println("Error, please input +, -, /, or x");
+        userInputFunction = userInputScannerObj.nextLine();
+      }
+    }
+  }
+
+  public static boolean checkOperator(String userInputFunction) {
     // cycle through possible operators
     switch (userInputFunction) {
       case "+":
