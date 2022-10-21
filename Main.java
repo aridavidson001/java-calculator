d867d0e9e77fa9b457732ada22e044286de3b0cd
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-10-21 15:21:21 -0400

    makes it loop over and over

--- Main.java
+++ Main.java
@@ -2,35 +2,34 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    // setup - decleration of variables, importing other classes, etc.
-    // declares variables for user input
-    double userInputFirstNumber;
-    String userInputFunction;
-    double userInputSecondNumber;
-    // creates a scanner function
-    Scanner userInputScannerObj = new Scanner(System.in);
-    // imports other classes
-    CallMaths callMaths = new CallMaths();
-    CheckOperator checkOperator = new CheckOperator();
+    for (;;) {
+      // setup - decleration of variables, importing other classes, etc.
+      // declares variables for user input
+      double userInputFirstNumber;
+      String userInputFunction;
+      double userInputSecondNumber;
+      // creates a scanner function
+      Scanner userInputScannerObj = new Scanner(System.in);
+      // imports other classes
+      CallMaths callMaths = new CallMaths();
+      CheckOperator checkOperator = new CheckOperator();
 
-    // takes input from user
-    System.out.println("Input First Number");
-    userInputFirstNumber = userInputScannerObj.nextDouble();
-    System.out.println(
-        "Add(+), Subtract(-), Divide(/), Multiply(x), or Modulus(%)?");
-    userInputFunction = userInputScannerObj.nextLine();
-    userInputFunction = userInputScannerObj.nextLine();
-    // loops the error untill a satisfactory character is input
-    checkOperator.checkOperatorFunc(userInputFunction);
+      // takes input from user
+      System.out.println("Input First Number");
+      userInputFirstNumber = userInputScannerObj.nextDouble();
+      System.out.println(
+          "Add(+), Subtract(-), Divide(/), Multiply(x), or Modulus(%)?");
+      userInputFunction = userInputScannerObj.nextLine();
+      userInputFunction = userInputScannerObj.nextLine();
+      // loops the error untill a satisfactory character is input
+      checkOperator.checkOperatorFunc(userInputFunction);
 
-    System.out.println("Input Second Number");
-    userInputSecondNumber = userInputScannerObj.nextDouble();
-
-    for (int rep = 0; rep < 10; rep++) {
-      System.out.println();
+      System.out.println("Input Second Number");
+      userInputSecondNumber = userInputScannerObj.nextDouble();
+      
+      // calls the correct function based on the userInputFunction
+      callMaths.callMaths(
+          userInputFirstNumber, userInputFunction, userInputSecondNumber);
     }
-    // calls the correct function based on the userInputFunction
-    callMaths.callMaths(
-        userInputFirstNumber, userInputFunction, userInputSecondNumber);
   }
 }
