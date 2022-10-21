f4542aad383b0ee0ec73f3a573e278a0e83bc261
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-10-21 09:42:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,38 +2,39 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-//setup - decleration of variables, importing other classes, etc.
-     //declares variables for user input
-     double userInputFirstNumber;
-     String userInputFunction;
-     double userInputSecondNumber;
-    //creates a scanner function
-    Scanner userInputScannerObj = new Scanner(System.in); 
-    //imports other classes
+    // setup - decleration of variables, importing other classes, etc.
+    // declares variables for user input
+    double userInputFirstNumber;
+    String userInputFunction;
+    double userInputSecondNumber;
+    // creates a scanner function
+    Scanner userInputScannerObj = new Scanner(System.in);
+    // imports other classes
     Maths maths = new Maths();
     CheckOperator checkOperator = new CheckOperator();
-   
-//takes input from user
+
+    // takes input from user
     System.out.println("Input First Number");
-     userInputFirstNumber = userInputScannerObj.nextDouble();
-    System.out.println("Add(+), Subtract(-), Divide(/), Multiply(x), or Modulus(%)?");
-     userInputFunction = userInputScannerObj.nextLine();
-     userInputFunction = userInputScannerObj.nextLine();
-     //loops the error untill a satisfactory character is input
-     if(checkOperator.checkOperator(userInputFunction) == false){
-  while(checkOperator.checkOperator(userInputFunction) == false){
-                      System.out.println("Error, please input +, -, /, or x");
-     userInputFunction = userInputScannerObj.nextLine();
+    userInputFirstNumber = userInputScannerObj.nextDouble();
+    System.out.println(
+        "Add(+), Subtract(-), Divide(/), Multiply(x), or Modulus(%)?");
+    userInputFunction = userInputScannerObj.nextLine();
+    userInputFunction = userInputScannerObj.nextLine();
+    // loops the error untill a satisfactory character is input
+    if (checkOperator.checkOperator(userInputFunction) == false) {
+      while (checkOperator.checkOperator(userInputFunction) == false) {
+        System.out.println("Error, please input +, -, /, or x");
+        userInputFunction = userInputScannerObj.nextLine();
       }
-     }
+    }
 
     System.out.println("Input Second Number");
-     userInputSecondNumber = userInputScannerObj.nextDouble();
-     
-        for(int rep = 0; rep < 10; rep++) {
-            System.out.println();
-}
-//calls the correct function based on the userInputFunction
+    userInputSecondNumber = userInputScannerObj.nextDouble();
+
+    for (int rep = 0; rep < 10; rep++) {
+      System.out.println();
+    }
+    // calls the correct function based on the userInputFunction
     switch (userInputFunction) {
       case "+":
         System.out.println(userInputFirstNumber + " " + userInputFunction + " "
@@ -67,4 +68,3 @@ public class Main {
     }
   }
 }
-
